40f120dc1a0fb7c4681a743bae54e984422aa656
joac001 <jordonez@fi.uba.ar>
2023-11-13 10:01:59 -0300

    Devops deployment in GH pages

--- src/components/componentExample.js
+++ src/components/componentExample.js
@@ -1,5 +1,17 @@
+import "../style/componentExample.css";
+
 export default function ComponentExample() {
     return (
-        <h1>Component example</h1>
+        <div className="welcome-text">
+
+            <h1>This is a Devops implementation of the pomodoro project.</h1>
+            <h3>This site is deployed in preparation for future versions of a project.</h3>
+            <br />
+
+            <img src={require('../assets/seeYouLater.gif')} alt="See you later alligator" className="gif" />
+
+        </div>
     );
 }
+
+
