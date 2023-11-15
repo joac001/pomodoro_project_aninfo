defae934cb75b1a92b516df3c3ee4e2e54f61e76
JM-Pascual <jmpascualosorio@gmail.com>
2023-11-15 13:48:06 -0300

    Base for the Timer class Base the Timer class and it's renderization, without stopping criteria

---
+++ src/components/timer.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import '../style/timer.css';
+
+class Timer extends Component {
+    constructor(props) {
+      super(props);
+  
+      // Initial state
+      this.state = {
+        mode: 'pomodoro',
+        time: props.timer.pomodoro * 60, //Time in in seconds
+        isRunning: false,
+      };
+  
+      //Interval variable for updating the timer
+      this.timerInterval = null;
+    }
+  
+    startTimer = () => {
+      if (this.state.isRunning) return; //Locks the user for pressing start multiple times
+
+      this.setState({ isRunning: true });
+        
+      this.timerInterval = setInterval(() => {
+        this.setState((prevState) => ({
+          time: prevState.time - 1,
+        }));
+
+      }, 1000);
+    };
+  
+    render() {
+      const { mode, time } = this.state;
+  
+      return (
+        <div className="timer-container">
+          <div className="timer-mode">{mode}</div>
+          <div className="timer-time">
+            {Math.floor(time / 60)}:{(time % 60).toLocaleString('en-US', { minimumIntegerDigits: 2 })}
+          </div>
+          <div className="timer-buttons">
+            { 
+            <button onClick={this.startTimer}>Start</button>
+            }
+          </div>
+        </div>
+      );
+    }
+  }
+  
+export default Timer;
