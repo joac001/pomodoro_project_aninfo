825677d081271102ee61f52ae56d1e0d20193760
Alejandro Vargas <alevargas.99.av@gmail.com>
2023-11-19 02:41:13 -0300

    Assigned styles to Delete Task button

--- src/components/taskCard.js
+++ src/components/taskCard.js
@@ -3,7 +3,11 @@ export default function TaskCard({ deleteTask, task }) {
         <div className="card mb-2 bg-primary">
             <div className="card-body">
                 <p className="card-text text-white">{ task.title }</p>
-                <button onClick={ () => deleteTask(task.id) }>Delete Task</button>
+                <button className="btn btn-light btn-delete" onClick={ () => deleteTask(task.id) }>
+                    <span className="material-symbols-outlined" id="inputGroup-sizing-default">
+                        delete
+                    </span>
+                </button>
             </div>
         </div>
     )
