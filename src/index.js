d3a7fa0c73c19b28345ea3caf7bb7e081d62f758
joac001 <jordonez@fi.uba.ar>
2023-11-20 16:22:17 -0300

    adding TaskContainer to index.js because they weren't there - adding the style file to taskContainer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 
 import './style/index.css';
 import Timer from './components/timer'; // Adjust the import path
+import TaskContainer from './components/taskContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <div>
-      <Timer timer={{ pomodoro: 25, break: 5 }} />
-    </div>
+
+    <Timer timer={{ pomodoro: 25, break: 5 }} />
+    <TaskContainer />
+
   </React.StrictMode>
 );
