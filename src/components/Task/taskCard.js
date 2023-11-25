58ccf9454ed7c0ba5cd4a16360c01cc54920e508
joac001 <jordonez@fi.uba.ar>
2023-11-25 14:56:54 -0300

    refactor styles

--- src/components/Task/taskCard.js
+++ src/components/Task/taskCard.js
@@ -2,7 +2,7 @@ export default function TaskCard({ deleteTask, task }) {
     return (
 
         <div className="card-body">
-            <p className="task-title">{task.title}</p>
+            <p>{task.title}</p>
             <span
                 className="material-symbols-outlined icon-delete"
                 id="inputGroup-sizing-default"
--- src/components/Task/taskList.js
+++ src/components/Task/taskList.js
@@ -1,24 +1,19 @@
 import TaskCard from "./taskCard";
 
 export default function TaskList({ deleteTask, tasks }) {
-    // initial state of taskList component with no tasks to display
-    try {
-        if (tasks.length === 0) {
-            return <div id="task-list">
+    return (
+        // initial state of taskList component with no tasks to display
+        TaskList.length === 0 ?
+            <div id="task-list">
                 <p className="no-task-added">No tasks added</p>
             </div>
-        }
-    } catch (e) {
-        return;
-    }
-
-    // the map method allows for element creation iterating on the given array
-    // key property is needed to avoid a specific compiler warning, is determined by the array's length
-    return (
-        <div id="task-list">
-            {tasks.map((task) => (
-                <TaskCard deleteTask={deleteTask} key={task.id} task={task} />
-            ))}
-        </div>
+            :
+            // the map method allows for element creation iterating on the given array
+            // key property is needed to avoid a specific compiler warning, is determined by the array's length
+            <div id="task-list">
+                {tasks.map((task) => (
+                    <TaskCard deleteTask={deleteTask} key={task.id} task={task} />
+                ))}
+            </div>
     );
 }
